c974403604d57109c68db3f040477aca891c2f9a
PARLI4MENT <ilya6joker@gmail.com>
2024-06-17 20:12:04 +1000

    #

--- PostgresSQL_Test/Program.cs
+++ PostgresSQL_Test/Program.cs
@@ -22,17 +22,18 @@ namespace mainNS
             await conn.OpenAsync();
 
             if (conn.State == System.Data.ConnectionState.Open)
-                Console.WriteLine("State = is Open");
+                Console.WriteLine("State => is Open");
             else
-                Console.WriteLine("State = wasn`t Open");
+                Console.WriteLine("State => wasn`t Open");
 
-            //InsertData(conn);
-            RetriveRow(conn);
+            InsertData(conn);
+            //RetriveRow(conn);
         }
 
         private async static void InsertData(NpgsqlConnection sqlConnection)
         {
-            await using (var command = new NpgsqlCommand("INSERT INTO Student(FIO_client, Number_client, Record) VALUES (@fio, @numb, @rec)", sqlConnection))
+            //await using (var command = new NpgsqlCommand("INSERT INTO Student (FIO_client, Number_client, Record ) VALUES ('Test1', 'Test1', 'Test' );", sqlConnection))
+            await using (var command = new NpgsqlCommand("INSERT INTO testDB (Name) VALUES (\"Test 1\");", sqlConnection))
             {
                 //var params[] = new NpgsqlParameter("fio", "test");
                 //await command.Parameters.Add();
