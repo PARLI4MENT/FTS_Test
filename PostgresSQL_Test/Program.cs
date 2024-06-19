7f7d51fd8f6251560478c6ae2fad0e5a1672080d
PARLI4MENT <ilya6joker@gmail.com>
2024-06-19 12:41:43 +1000

    # Commit

--- PostgresSQL_Test/Program.cs
+++ PostgresSQL_Test/Program.cs
@@ -2,12 +2,15 @@
 using Npgsql;
 using System;
 using System.Runtime.CompilerServices;
+using System.Configuration;
 
 namespace mainNS
 {
     public class Program
     {
-        const string strConnection = "Server=192.168.0.142;Port=5438;Database=student;User ID=postgres;Password=passwd0105";
+        const string pgStrConn = "Server=192.168.0.142;Port=5438;Database=student;User ID=postgres;Password=passwd0105";
+
+
 
         public static void Main(string[] args)
         {
@@ -18,41 +21,55 @@ namespace mainNS
 
         public async static void PGConnect()
         {
-            await using var conn = new NpgsqlConnection(strConnection);
-            await conn.OpenAsync();
+            await using var sqlConnection = new NpgsqlConnection(pgStrConn);
+            await sqlConnection.OpenAsync();
 
-            if (conn.State == System.Data.ConnectionState.Open)
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
                 Console.WriteLine("State => is Open");
             else
                 Console.WriteLine("State => wasn`t Open");
 
-            InsertData(conn);
+            //InsertData(conn);
             //RetriveRow(conn);
+
+            DataReader(sqlConnection);
         }
 
-        private async static void InsertData(NpgsqlConnection sqlConnection)
+        private async static void DataReader(NpgsqlConnection sqlConnection)
         {
-            //await using (var command = new NpgsqlCommand("INSERT INTO Student (FIO_client, Number_client, Record ) VALUES ('Test1', 'Test1', 'Test' );", sqlConnection))
-            await using (var command = new NpgsqlCommand("INSERT INTO testDB (Name) VALUES (\"Test 1\");", sqlConnection))
+            try
             {
-                //var params[] = new NpgsqlParameter("fio", "test");
-                //await command.Parameters.Add();
-                await command.ExecuteNonQueryAsync();
+
             }
+            catch (Exception ex) { Console.WriteLine(ex.Message); return; }
+
+            throw new Exception();
         }
 
-        private async static void RetriveRow(NpgsqlConnection sqlConnection)
-        {
-            await using (var command = new NpgsqlCommand("SELECT * FROM Student", sqlConnection))
-            {
-                await using (var reader = await command.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        Console.WriteLine(reader.GetString(0));
-                    }
-                }
-            }
-        }
+
+        //private async static void InsertData(NpgsqlConnection sqlConnection)
+        //{
+        //    //await using (var command = new NpgsqlCommand("INSERT INTO Student (FIO_client, Number_client, Record ) VALUES ('Test1', 'Test1', 'Test' );", sqlConnection))
+        //    await using (var command = new NpgsqlCommand("INSERT INTO testDB (Name) VALUES (\"Test 1\");", sqlConnection))
+        //    {
+        //        //var params[] = new NpgsqlParameter("fio", "test");
+        //        //await command.Parameters.Add();
+        //        await command.ExecuteNonQueryAsync();
+        //    }
+        //}
+
+        //private async static void RetriveRow(NpgsqlConnection sqlConnection)
+        //{
+        //    await using (var command = new NpgsqlCommand("SELECT * FROM Student", sqlConnection))
+        //    {
+        //        await using (var reader = await command.ExecuteReaderAsync())
+        //        {
+        //            while (await reader.ReadAsync())
+        //            {
+        //                Console.WriteLine(reader.GetString(0));
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
