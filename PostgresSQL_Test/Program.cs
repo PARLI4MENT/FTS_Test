fb644dc5b21f383933f67d642f59c6bdf470af88
PARLI4MENT <ilya6joker@gmail.com>
2024-06-24 14:33:39 +1000

    #

--- PostgresSQL_Test/Program.cs
+++ PostgresSQL_Test/Program.cs
@@ -14,73 +14,93 @@ namespace MainNS
 
         public static void Main(string[] args)
         {
-            PGConnect();
+            PgConnect();
 
             Console.ReadKey();
         }
 
-        public async static void PGConnect()
+        public async static void PgConnect()
         {
-            await using var sqlConnection = new NpgsqlConnection(strConn);
-            await sqlConnection.OpenAsync();
+            // Connection
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(strConn);
+            var dataSource = dataSourceBuilder.Build();
+            var sqlConnection = await dataSource.OpenConnectionAsync();
 
             if (sqlConnection.State == System.Data.ConnectionState.Open)
                 Console.WriteLine("State => is Open");
             else
                 Console.WriteLine("State => wasn`t Open");
 
-            DataReader(sqlConnection);
-            //RetriveRow(conn);
-            //InsertData(conn);
+
+            PgCheckDb(sqlConnection);
+
+            //RetriveData(sqlConnection);
+            //InsertData(sqlConnection);
         }
 
-        private async static void DataReader(NpgsqlConnection sqlConnection)
+
+        private async static void PgCheckDb(NpgsqlConnection sqlConnection)
+        {
+            const string tableInfo = "SELECT * FROM information_schema.tables";
+
+            try
+            {
+                await using (var sqlComm = new NpgsqlCommand(tableInfo, sqlConnection))
+                {
+                    await using (var reader = sqlComm.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("Now tables");
+                            return;
+                        }
+                        while (await reader.ReadAsync())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); return ; }
+
+        }
+
+        private async static void PgInsertData(NpgsqlConnection sqlConnection)
         {
             try
             {
-                NpgsqlCommand sqlComm = new NpgsqlCommand();
-                sqlComm.Connection = sqlConnection;
-                sqlComm.CommandType = System.Data.CommandType.Text;
-                sqlComm.CommandText = "SELECT * FROM \"public\".\"Student\"";
-
-                NpgsqlDataReader dataReader = sqlComm.ExecuteReader();
-
-                if (dataReader.HasRows)
+                // Insert
+                await using (var sqlComm = new NpgsqlCommand("SELECT * FROM \"public\".\"Student\"", sqlConnection))
                 {
-                    DataTable dataTable = new DataTable();
-                    dataTable.Load(dataReader);
+
                 }
 
-                sqlComm.Dispose();
-                sqlConnection.Close();                
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); return; }
         }
 
+        private async static void PgRetriveData(NpgsqlConnection sqlConnection)
+        {
 
-        //private async static void InsertData(NpgsqlConnection sqlConnection)
-        //{
-        //    //await using (var command = new NpgsqlCommand("INSERT INTO Student (FIO_client, Number_client, Record ) VALUES ('Test1', 'Test1', 'Test' );", sqlConnection))
-        //    await using (var command = new NpgsqlCommand("INSERT INTO testDB (Name) VALUES (\"Test 1\");", sqlConnection))
-        //    {
-        //        //var params[] = new NpgsqlParameter("fio", "test");
-        //        //await command.Parameters.Add();
-        //        await command.ExecuteNonQueryAsync();
-        //    }
-        //}
+            await using (var sqlComm = new NpgsqlCommand("SELECT * FROM \"public\".\"Student\"", sqlConnection))
+            {
+                await using (NpgsqlDataReader reader = await sqlComm.ExecuteReaderAsync())
+                {
+                    if (!reader.HasRows)
+                    {
+                        Console.WriteLine("No rows!");
+                        return;
+                    } 
 
-        //private async static void RetriveRow(NpgsqlConnection sqlConnection)
-        //{
-        //    await using (var command = new NpgsqlCommand("SELECT * FROM Student", sqlConnection))
-        //    {
-        //        await using (var reader = await command.ExecuteReaderAsync())
-        //        {
-        //            while (await reader.ReadAsync())
-        //            {
-        //                Console.WriteLine(reader.GetString(0));
-        //            }
-        //        }
-        //    }
-        //}
+                    while (await reader.ReadAsync())
+                    {
+                        Console.WriteLine(reader.GetString(0));
+                    }
+                }
+            }
+        }
+
+
     }
 }
