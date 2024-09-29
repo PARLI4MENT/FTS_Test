4e43dcdfd4a59b81180ab972b0b76cda475d5092
MainPC <ilya6joker@gmail.com>
2024-09-29 23:32:03 +1000

    # Realise IDispose in ViewModel

---
+++ FTS_XML_UI_Netframework/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTS_XML_UI_Netframework.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+    }
+}
